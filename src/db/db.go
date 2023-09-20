505b79008a07f5593eeff652d206d79a0f1197bb
Egehan <egehanavcu@gmail.com>
2023-09-20 17:43:47 +0300

    Added NowFunc again

--- src/db/db.go
+++ src/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/GokdenizCakir/stant_oyun/src/models"
 	"gorm.io/driver/postgres"
@@ -13,7 +14,12 @@ var DB *gorm.DB
 func Init() {
 	var err error
 	dsn := "host=trumpet.db.elephantsql.com user=fxwnvahh password=3AOt6mw6wPENHWSwHNcMyVDiVBhuGQyb dbname=fxwnvahh port=5432 sslmode=require TimeZone=Europe/Istanbul"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NowFunc: func() time.Time {
+			ti, _ := time.LoadLocation("Europe/Istanbul")
+			return time.Now().In(ti)
+		},
+	})
 	if err != nil {
 		panic("failed to connect database")
 	}
