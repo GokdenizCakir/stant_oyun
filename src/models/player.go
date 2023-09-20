2c72020d1d55deade7d817f2c1e24f9efb2ab530
Egehan <egehanavcu@gmail.com>
2023-09-20 17:06:02 +0300

    Added creation time to players table

--- src/models/player.go
+++ src/models/player.go
@@ -1,10 +1,13 @@
 package models
 
+import "time"
+
 type Player struct {
-	ID          uint   `gorm:"primaryKey"`
-	FullName    string `json:"full_name" gorm:"size:40"`
-	Phone       string `json:"phone" gorm:"unique"`
-	IP          string `json:"ip"` /* gorm:"unique" */
-	Score       int    `json:"score" gorm:"default:0"`
-	HasFinished bool   `json:"has_finished" gorm:"default:false"`
+	ID          uint      `gorm:"primaryKey"`
+	FullName    string    `json:"full_name" gorm:"size:40"`
+	Phone       string    `json:"phone" gorm:"unique"`
+	IP          string    `json:"ip"` /* gorm:"unique" */
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
+	Score       int       `json:"score" gorm:"default:0"`
+	HasFinished bool      `json:"has_finished" gorm:"default:false"`
 }
