b4d2bbc753dc69d63948ff87c092b76faaefea6f
Egehan <egehanavcu@gmail.com>
2023-10-01 19:49:01 +0300

    Added duplicate IP check again

--- src/models/player.go
+++ src/models/player.go
@@ -4,7 +4,7 @@ type Player struct {
 	ID           uint   `gorm:"primaryKey"`
 	FullName     string `json:"full_name" gorm:"size:40"`
 	Phone        string `json:"phone" gorm:"unique"`
-	IP           string `json:"ip"` /* gorm:"unique" */
+	IP           string `json:"ip" gorm:"unique"`
 	CreationTime string `json:"creation_time"`
 	Score        int    `json:"score" gorm:"default:0"`
 	HasFinished  bool   `json:"has_finished" gorm:"default:false"`
